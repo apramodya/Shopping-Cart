db8684c50a9891cae655f214bf5a3a3e1b500c20
apramodya <pramodyaabeysinghe@gmail.com>
2018-04-12 16:52:56 +0530

    Add user registration with CSRF Token

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');
 
-/* GET home page. */
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
+// GET home page
 router.get('/', function (req, res, next) {
     var products = Product.find(function (err, docs) {
         res.render('shop/index', {
@@ -11,5 +15,17 @@ router.get('/', function (req, res, next) {
         });
     });
 });
+
+// GET signup page
+router.get('/user/signup', function (req, res, next) {
+    res.render('user/signup', {
+        csrfToken: req.csrfToken()
+    });
+});
+// POST signup page
+router.post('/user/signup', function (req, res, next) {
+    res.redirect('/');
+});
+
 
 module.exports = router;
